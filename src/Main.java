fa3f824ff52fb96f318c9c25db0dcafc11b2eb34
topsy <topsytrabajo@gmail.com>
2026-01-08 18:56:11 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Bienvenid@ al Conversor de Moneda!");
+        String opciones = """
+                1) Dólar => Peso Argentino
+                2) Peso Argentino => Dólar
+                3) Dólar => Real Brasileño
+                4) Real Brasileño => Dólar
+                5) Dólar => Peso Colombiano
+                6) Peso Colombiano => Dólar
+                7) Salir
+                
+                """;
+        System.out.println(opciones);
+        System.out.println("Elija una opcion valida");
+
+
     }
 }
