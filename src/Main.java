ee1d0c852bf1e847d9d99aa3b4d0fce4e318ba7e
topsy <topsytrabajo@gmail.com>
2026-01-09 02:23:48 -0300

    inclusion de hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.sql.SQLOutput;
-import java.util.Arrays;
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,37 +8,52 @@ public class Main {
     public static void main(String[] args) {
         Scanner ingreso = new Scanner(System.in);
         ConversorService conversorService = new ConversorService();
+        HashMap<Integer, String> listaConversiones = new HashMap<>();
+        listaConversiones.put(1,"USD ARS");
+        listaConversiones.put(2,"ARS USD");
+        listaConversiones.put(3,"USD BRL");
+        listaConversiones.put(4,"BRL USD");
+        listaConversiones.put(5,"USD COP");
+        listaConversiones.put(6,"COP USD");
 
 
         System.out.println("Bienvenid@ al Conversor de Moneda!");
-        String opciones = """
-                1) USD => ARS (Dólar => Peso Argentino)
-                2) ARS => USD (Peso Argentino => Dólar)
-                3) USD => BRL (Dólar => Real Brasileño)
-                4) BRL => USD (Real Brasileño => Dólar)
-                5) USD => COP (Dólar => Peso Colombiano)
-                6) COP => USD (Peso Colombiano => Dólar)
-                7) Salir
-                
-                """;
-        System.out.println(opciones);
-        System.out.println("Elija una opción válida");
-        String elegido = ingreso.nextLine();
-        System.out.println("Ingrese el valor que desea convertir");
-        double monto = ingreso.nextDouble();
-        ingreso.nextLine();
+        while(true) {
+            String opciones = """
+                    1) Dólar => Peso Argentino
+                    2) Peso Argentino => Dólar
+                    3) Dólar => Real Brasileño
+                    4) Real Brasileño => Dólar
+                    5) Dólar => Peso Colombiano
+                    6) Peso Colombiano => Dólar
+                    7) Salir
+                    
+                    """;
+            System.out.println(opciones);
+            System.out.println("Elija una opción válida");
+            int elegido = ingreso.nextInt();
 
-        String[] oraciones = opciones.split("\n");
-
-        for(int i = 0; i<oraciones.length; i++){
-            String[] linea = oraciones[i].split(" ");
-            if (linea[0].charAt(0) == elegido.charAt(0)){
-                String base = linea[1].trim();
-                String target = linea[3].trim();
-                Exchange conversion = conversorService.conversionPar(base,target,monto);
-                System.out.println("El valor "+monto+" "+base+" corresponde al valor final de "+conversion.conversion_result()+" "+target);
+            if(elegido == 7){
                 break;
             }
+
+            if(listaConversiones.containsKey(elegido)){
+                System.out.println("Ingrese el valor que desea convertir");
+                double amount = ingreso.nextDouble();
+                ingreso.nextLine();
+                String[] currency = (listaConversiones.get(elegido)).split(" ");
+                String base = currency[0].trim();
+                String target = currency[1].trim();
+                Exchange conversion = conversorService.conversionPar(base,target,amount);
+                System.out.println("El valor "+amount+" "+base+" corresponde al valor final de "+conversion.conversion_result()+" "+target);
+
+
+            }else{
+                System.out.println("Opcion inválida, intente nuevamente");
+            }
+            System.out.println();
+        }
+
             }
         }
 
@@ -47,4 +62,4 @@ public class Main {
 
 
 
-    }
+
